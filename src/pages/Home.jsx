0e16fecdeb3a263a18de4747b12b0d7db962aa06
Murat Altun <mraltun@gmail.com>
2022-10-18 17:16:24 +0300

    Testing API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect } from "react";
-import { fetchMarvel, QUERY } from "../api/utils";
+import React, { useState, useEffect } from "react";
+import { fetchMarvel } from "../api/utils";
 
 const Home = () => {
-  //   useEffect(() => {
-  //     const getChars = async () => {
-  //       try {
-  //         const response = await fetchMarvel(`/characters`);
-  //         const data = await response.data.data.results;
-  //         console.log(data);
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     };
-  //     getChars();
-  //   }, []);
+  const [chars, setChars] = useState();
 
-  return <div>Home</div>;
+  useEffect(() => {
+    const getChars = async () => {
+      try {
+        const response = await fetchMarvel(`/characters`);
+        const data = await response.data.data.results;
+        console.log(data);
+        setChars(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getChars();
+  }, []);
+
+  return (
+    <main>
+      <div>
+        {chars &&
+          chars.map((hero) => (
+            <div key={hero.id}>
+              <h4>{hero.name}</h4>
+            </div>
+          ))}
+      </div>
+    </main>
+  );
 };
 
 export default Home;
