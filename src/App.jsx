aa1860d056ad513a12f36275510ce47992baf6fd
Murat Altun <mraltun@gmail.com>
2022-10-19 14:44:24 +0300

    Navbar and it's css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
 import Home from "./pages/Home";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import MarvelLogo from "../assets/marvelLogo.png";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styles.navbar}>
+      <img src={FaBars} className={styles.hamburgerMenu} alt='' />
+      <NavLink className={styles.navlinkLogo} to='/'>
+        <img src='../assets/marvelLogo.png' alt='logo' />
+      </NavLink>
+      <div className={styles.navbarLinks}>
+        <NavLink
+          className={(isActive) => (isActive ? styles.navlink : undefined)}
+          to='about'
+        >
+          About
+        </NavLink>
+        <NavLink className={styles.navlink} to='services'>
+          Services
+        </NavLink>
+        <NavLink className={styles.navlink} to='contact-us'>
+          Contact Us
+        </NavLink>
+        <NavLink className={styles.navlink} to='sign-up'>
+          Sign Up
+        </NavLink>
+        {/* Second Nav */}
+        {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
