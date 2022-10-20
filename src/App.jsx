0121f9d4f0c53333c9b7f3afc87694f363fd77d9
Murat Altun <mraltun@gmail.com>
2022-10-20 19:24:01 +0300

    Working on NewNavbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import NewNavbar from "./components/NewNavbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
+      <NewNavbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
---
+++ src/components/NewNavbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/marvelLogo.png";
+import styles from "./NewNavbar.module.css";
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <div className={styles.navbar}>
+      <div className='logo'>
+        <img src={Logo} alt='logo' />
+      </div>
+      <ul
+        className={navbarOpen ? `${styles.navMenu} active` : styles.navMenu}
+        onClick={() => setNavbarOpen(false)}
+      >
+        <li className={styles.navItem}>
+          <Link to='/'>Home</Link>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink to='characters'>Characters</NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink to='comics'>Comics</NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink to='about'>About</NavLink>
+        </li>
+      </ul>
+      <div className='hamburger' onClick={() => setNavbarOpen(!navbarOpen)}>
+        {navbarOpen ? (
+          <FaTimes size={30} style={{ color: "#f8f8f8" }} />
+        ) : (
+          <FaBars size={30} style={{ color: "#f8f8f8" }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
