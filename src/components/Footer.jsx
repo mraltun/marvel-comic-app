e735f95b6af9c53498940090de724d2f5e36758d
Murat Altun <mraltun@gmail.com>
2022-10-28 15:06:01 +0300

    Footer jsx and it's css changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,17 +7,26 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <Link to='https://github.com/mraltun'>
+        <a href='https://github.com/mraltun' target='_blank' rel='noreferrer'>
           <FaGithub className={styles.icons} />
-        </Link>
-        <Link to='https://www.linkedin.com/in/mraltun/'>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/mraltun/'
+          target='_blank'
+          rel='noreferrer'
+        >
           <FaLinkedin className={styles.icons} />
-        </Link>
-        <Link to='https://twitter.com/mraltun62'>
+        </a>
+        <a
+          href='https://twitter.com/mraltun62'
+          target='_blank'
+          rel='noreferrer'
+        >
           <FaTwitter className={styles.icons} />
-        </Link>
+        </a>
+        <span> - </span>
+        <p>&copy; 2022 Murat Altun.</p>
       </div>
-      <p className={styles.copyright}>&copy; 2022 Murat Altun.</p>
     </footer>
   );
 };
