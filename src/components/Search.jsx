798720447586c3f025cbac44dc2215fa978a79db
Murat Altun <mraltun@gmail.com>
2022-10-28 17:23:27 +0300

    Search css improvements

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import styles from "./Search.module.css";
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -12,12 +13,18 @@ const Search = () => {
   };
 
   return (
-    <div>
-      <FaSearch />
-      <form>
-        <input type='text' placeholder='Search' />
-      </form>
-    </div>
+    <form className={styles.form} onSubmit={submitHandler}>
+      <div className={styles.search}>
+        <FaSearch className={styles.icon} />
+        <input
+          className={styles.input}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          type='text'
+          value={searchTerm}
+          placeholder='Search...'
+        />
+      </div>
+    </form>
   );
 };
 
